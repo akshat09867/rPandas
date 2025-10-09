60f2b59f171f00feb6dc2a0852f03aa1814c7ea8
akshat09867 <codingmaster902@gmail.com>
2025-10-09 15:51:25 +0530

    update @docType

--- R/rPandas-package.R
+++ R/rPandas-package.R
@@ -3,7 +3,9 @@
 #' This package provides a set of wrapper functions that allow R users to
 #' interact with Python's pandas library using familiar R syntax.
 #'
-#' @docType _package
+#' @docType package
 #' @name rPandas-package
+#' @aliases rPandas
 #' @import reticulate
+#' @keywords package
 NULL
